52631defc23fbcffc556d127b50f01ef553d537b
Aline Galvão <alinegs93@gmail.com>
2022-08-16 22:06:51 -0300

    adicionando o DELETE

--- js/excluir-evento.js
+++ js/excluir-evento.js
@@ -1,55 +1,28 @@
 const SOUND_URL = "https://xp41-soundgarden-api.herokuapp.com/";
 
-const inputNome = document.getElementById("nome");
-const inputAtracoes = document.getElementById("atracoes");
-const inputDescricao = document.getElementById("descricao");
-const inputData = document.getElementById("data");
-const inputLotacao = document.getElementById("lotacao");
-const inputBanner = document.getElementById("banner");
-const inputForm = document.getElementById("form");
-const id = new URLSearchParams(window.location.search).get("id");
+const inputNome = document.querySelector("nome");
+const inputAtracoes = document.querySelector("atracoes");
+const inputDescricao = document.querySelector("descricao");
+const inputData = document.querySelector("data");
+const inputLotacao = document.querySelector("lotacao");
+const inputForm = document.querySelector("form");
 
-async function listarEvento() {
-  const options = {
-    method: "GET",
-    redirect: "follow",
-    headers: { "Content-Type": "application/json" },
-  };
 
-  const resposta = await fetch(`${SOUND_URL}/events/${id}`, options);
-
-  const conteudo = await resposta.json();
-  inputNome.value = conteudo.name;
-  inputBanner.value = conteudo.poster;
-  inputAtracoes.value = conteudo.attractions;
-  inputDescricao.value = conteudo.description;
-  inputData.value = conteudo.scheduled.slice(0, 16);
-  inputLotacao.value = conteudo.number_tickets;
-}
-listarEvento();
-
-form.onsubmit = async (evento) => {
-  evento.preventDefault();
-
-  const optionsDel = {
-    method: "DELETE",
-    body: JSON.stringify(apagarEvento),
+const excluirEvento = async () => {
+  const respostaExcluir = await fetch(`${SOUND_URL}/events/${id}`, {
+    method: "DEL",
     headers: { "Content-Type": "application/json" },
     redirect: "follow",
-  };
-
-  // const btn = () => {
-  //   btn.document
-  //     .getElementsByClassName("btn")
-  //     .RemoveEventListener("click", listener, false);
-  // };
-
-  const resposta = await fetch(`${SOUND_URL}/events/${id}`, optionsDel);
-  if (resposta.status == 204) {
-    alert("Evento excluido!");
-    window.location.pathname == "/SoundGarden/excluir-evento.html"
-      ? `${window.location.origin}/SoundGarden/admin.html`
-      : `${window.location.origin}/admin.html`;
+  })
+ 
   }
+  excluirEvento();
+  
+  const btnExcluirEvento = document.querySelector(".btn.btn-danger");
+btnExcluirEvento.onsubmit = (evento) => {
+  evento.preventDefault();
+  
 };
-
+console.log(btnExcluirEvento);
+console.log(alert("Evento Excluído"));  
+  
