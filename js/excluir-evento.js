7f735bd1597e879f8ba92587bc78a9b3898cd196
Aline Galvão <alinegs93@gmail.com>
2022-08-17 20:44:03 -0300

    alteração

--- js/excluir-evento.js
+++ js/excluir-evento.js
@@ -1,16 +1,11 @@
 const SOUND_URL = "https://xp41-soundgarden-api.herokuapp.com/";
 
-const inputNome = document.querySelector("nome");
-const inputAtracoes = document.querySelector("atracoes");
-const inputDescricao = document.querySelector("descricao");
-const inputData = document.querySelector("data");
-const inputLotacao = document.querySelector("lotacao");
 const inputForm = document.querySelector("form");
 
 
 const excluirEvento = async () => {
   const respostaExcluir = await fetch(`${SOUND_URL}/events/${id}`, {
-    method: "DEL",
+    method: "DELETE",
     headers: { "Content-Type": "application/json" },
     redirect: "follow",
   })
@@ -21,8 +16,7 @@ const excluirEvento = async () => {
   const btnExcluirEvento = document.querySelector(".btn.btn-danger");
 btnExcluirEvento.onsubmit = (evento) => {
   evento.preventDefault();
-  
+  alert('Evento excluído');  
 };
 console.log(btnExcluirEvento);
-console.log(alert("Evento Excluído"));  
-  
+
