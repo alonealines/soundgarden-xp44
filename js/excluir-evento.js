de393bad4aa3ea74d29d924d24e1463106b048ad
Aline Galvão <alinegs93@gmail.com>
2022-08-17 23:36:03 -0300

    alteração

--- js/excluir-evento.js
+++ js/excluir-evento.js
@@ -1,22 +1,66 @@
-const SOUND_URL = "https://xp41-soundgarden-api.herokuapp.com/";
-
-const inputForm = document.querySelector("form");
+const form = document.querySelector("form");
 
 
-const excluirEvento = async () => {
-  const respostaExcluir = await fetch(`${SOUND_URL}/events/${id}`, {
-    method: "DELETE",
-    headers: { "Content-Type": "application/json" },
-    redirect: "follow",
-  })
- 
-  }
-  excluirEvento();
-  
-  const btnExcluirEvento = document.querySelector(".btn.btn-danger");
-btnExcluirEvento.onsubmit = (evento) => {
-  evento.preventDefault();
-  alert('Evento excluído');  
+const SOUND_URL = "https://xp41-soundgarden-api.herokuapp.com/events";
+
+
+const findID = () => {
+  const url = new URL(window.location.href);
+  const id = url.searchParams.get("id");
+
+  return id;
 };
-console.log(btnExcluirEvento);
 
+
+const exibirDetalhesEvento = async () => {
+  const dadosEvento = await fetch("SOUND_URL" + "/" + findID(), {
+    method: "GET",
+    mode: "cors",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  }).then((response) => response.json());
+
+  console.log(dadosEvento);
+
+  const inputNome = document.getElementById("nome");
+  const inputAtracoes = document.getElementById("atracoes");
+  const inputDescricao = document.getElementById("descricao");
+  const inputData = document.getElementById("data");
+  const inputLotacao = document.getElementById("lotacao");
+  const inputBanner = document.getElementById("banner");
+
+  inputNome.value = dadosEvento.name;
+  inputAtracoes.value = dadosEvento.attractions;
+  inputBanner.value = dadosEvento.poster;
+  inputDescricao.value = dadosEvento.description;
+  inputData.value = dadosEvento.scheduled;
+  inputLotacao.value = dadosEvento.number_tickets;
+};
+exibirDetalhesEvento();
+
+const btnExcluirEvento = document.querySelector(".btn.btn-danger");
+btnExcluirEvento.addEventListener =
+  ("onClick",
+  async (event) => {
+    event.preventDefault();
+    
+
+    const resposta = await fetch("SOUND_URL" + "/" + findID(), {
+      method: "DELETE",
+      mode: "cors",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((response) => {
+        return response;
+      })
+      .then((responseOBJ) => {
+        //console.log(responseOBJ);
+        window.location.replace("admin.html?acao=edit");
+      });
+      alert("Evento excluído");
+  });
+
+  
