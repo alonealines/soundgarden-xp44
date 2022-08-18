03586846054c79551b586939b990205dd25f1639
Aline Galvão <alinegs93@gmail.com>
2022-08-18 13:13:06 -0300

    alteração no comando DELETE

--- js/excluir-evento.js
+++ js/excluir-evento.js
@@ -1,66 +1,56 @@
-const form = document.querySelector("form");
-
-
-const SOUND_URL = "https://xp41-soundgarden-api.herokuapp.com/events";
-
+const SOUND_URL = 'https://xp41-soundgarden-api.herokuapp.com/events';
 
 const findID = () => {
+
   const url = new URL(window.location.href);
-  const id = url.searchParams.get("id");
+  const id = url.searchParams.get('id');
 
   return id;
-};
-
+}
 
 const exibirDetalhesEvento = async () => {
-  const dadosEvento = await fetch("SOUND_URL" + "/" + findID(), {
-    method: "GET",
-    mode: "cors",
-    headers: {
-      "Content-Type": "application/json",
-    },
-  }).then((response) => response.json());
+  const dadosEvento =
+      await fetch('https://xp41-soundgarden-api.herokuapp.com/events/' + findID(), {
+          method: "GET",
+          mode: "cors",
+          headers: {
+              "Content-Type": "application/json"
+          }
+      }).then((response) => response.json());
 
   console.log(dadosEvento);
 
-  const inputNome = document.getElementById("nome");
-  const inputAtracoes = document.getElementById("atracoes");
-  const inputDescricao = document.getElementById("descricao");
-  const inputData = document.getElementById("data");
-  const inputLotacao = document.getElementById("lotacao");
-  const inputBanner = document.getElementById("banner");
+  const inputNome = document.querySelector("#nome");
+  const inputAtracoes = document.querySelector("#atracoes");
+  const inputDescricao = document.querySelector("#descricao");
+  const inputData = document.querySelector("#data");
+  const inputLotacao = document.querySelector("#lotacao");
+  const inputBanner = document.querySelector("#banner");
 
   inputNome.value = dadosEvento.name;
-  inputAtracoes.value = dadosEvento.attractions;
+  inputAtracoes.value = dadosEvento.attractions.join(', ');
   inputBanner.value = dadosEvento.poster;
   inputDescricao.value = dadosEvento.description;
   inputData.value = dadosEvento.scheduled;
   inputLotacao.value = dadosEvento.number_tickets;
-};
+}
+
 exibirDetalhesEvento();
 
-const btnExcluirEvento = document.querySelector(".btn.btn-danger");
-btnExcluirEvento.addEventListener =
-  ("onClick",
-  async (event) => {
-    event.preventDefault();
-    
+const deletarEvento = async () => {
 
-    const resposta = await fetch("SOUND_URL" + "/" + findID(), {
-      method: "DELETE",
-      mode: "cors",
-      headers: {
-        "Content-Type": "application/json",
-      },
-    })
-      .then((response) => {
-        return response;
-      })
-      .then((responseOBJ) => {
-        //console.log(responseOBJ);
-        window.location.replace("admin.html?acao=edit");
-      });
-      alert("Evento excluído");
-  });
+const resposta = await fetch('https://xp41-soundgarden-api.herokuapp.com/events/' + findID(), { 
+  method: "DELETE",
+  mode: "cors",
+  headers: {
+      "Content-type": "application/json"
+  },
+})
 
-  
+  .then(response => response)
+          .then(result => { window.location.href = './admin.html'})
+          .catch(error => alert(`O evento ${nome} não foi excluido!`));
+          alert('O evento foi excluido!');
+};
+
+deletarEvento()
