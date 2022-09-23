3f52558a8107c4d80f92906331b9ae22035a0869
Aline Galvão <alinegs93@gmail.com>
2022-09-22 21:04:46 -0300

    adicionando eventos

---
+++ js/eventos.js
@@ -0,0 +1,34 @@
+const SOUND_URL = 'https://xp41-soundgarden-api.herokuapp.com/events';
+
+const listarEventos = async () => {
+
+    const eventos = await fetch(SOUND_URL, {
+        method: "GET",
+        mode: "cors",
+        headers: {
+            "Content-Type": "application/json"
+        }
+
+    }).then((resposta) => {
+        return resposta.json();
+    });
+
+    console.log(eventos)
+
+    const eventList = document.querySelector('#all-events')
+
+    let htmlEventos = "";
+
+    eventos.forEach(evento => {
+      htmlEventos += `
+            <article class="evento card p-5 m-3">
+              <h2>${evento.name}</h2>
+              <h4>${evento.attractions.join(', ')}</h4>
+              <a href="#" class="btn btn-primary">reservar ingresso</a>
+            </article>
+      `; 
+  })
+  eventList.innerHTML = htmlEventos;
+}
+
+  listarEventos();
